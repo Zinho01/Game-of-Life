60ef29a4977a05a735e014069dccdd215cf0b6a9
Jair <jair3602017@outlook.com>
2024-08-27 09:47:32 +0200

    commit

---
+++ game.py
@@ -0,0 +1,21 @@
+import os 
+
+def make_commit(days: int):
+    if days <10 :
+        return os.system('git push')
+    else:
+        dates = f'{days} days ago'
+
+        with open('data.txt', 'a') as file:
+            file.write(f'{dates}\n')
+
+        #Staging
+        os.system('git add game.txt')
+        #Commit
+        os.system(f'git commit --date="'+dates+'" -m "commit"')
+        
+
+        return days * make_commit(days-10)
+    
+
+make_commit(10)
