1261b58378891ec27af3bd6a21bb0e361f1fb53f
Tafish Qureshi <126494656+Tafish-Qureshi@users.noreply.github.com>
2024-09-03 16:12:01 +0200

    Revert "BoardFix"

--- Start/script.js
+++ Start/script.js
@@ -1,34 +1,5 @@
 const canvas = document.getElementById('gamecanvas');
 const ctx = canvas.getContext('2d');
-const cellsize = 100;
-const rows = 20;
-const cols = 20;
-
-
-canvas.width = cols * cellsize;
-canvas.height = rows * cellsize;
-
-for (let i = 0; i < rows; i++) {
-  for (let j = 0; j < cols; j++) {
-    
-    const condition = (i + j) % 2 === 0; 
-
-
-    ctx.fillStyle = 'white';
-    ctx.fillRect(j * cellsize, i * cellsize, cellsize, cellsize);
-
-    //condition moet nog worden aangepast
-    if (condition) {
-        ctx.fillStyle = 'white';
-        ctx.fillRect(j * cellsize, i * cellsize, cellsize, cellsize);
-    }
-
-    ctx.strokesStyle = 'black';
-    ctx.lineWidth = 1;
-    ctx.strokeRect(j * cellsize, i * cellsize, cellsize, cellsize);
-  }
-}
-
-function clicker() {
-    
-}
+const cellsize = 10;
+canvas.width = 4 * cellsize;
+canvas.height =  6 * cellsize;
